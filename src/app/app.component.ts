7dcbbe87aee6553fb6dc73cd33d83a0e11bac0f0
Bodó Dávid <h848002@stud.u-szeged.hu>
2020-12-22 13:07:26 +0100

    pre admin features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firestore';
-import * as admin from 'firebase-admin';
+/*import * as admin from 'firebase-admin';
+import * as serviceAccount from '../../serviceAccountKey.json'*/
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,24 @@ import * as admin from 'firebase-admin';
 export class AppComponent implements OnInit{
   title = 'eraconverter-Web';
 
+  /*params = {
+    type: serviceAccount.type,
+    projectId: serviceAccount.project_id,
+    privateKeyId: serviceAccount.private_key_id,
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+    clientId: serviceAccount.client_id,
+    authUri: serviceAccount.auth_uri,
+    tokenUri: serviceAccount.token_uri,
+    authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+    clientC509CertUrl: serviceAccount.client_x509_cert_url
+  }*/
+
   constructor() {}
 
   ngOnInit(): void {
     /*admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
+      credential: admin.credential.cert(this.params),
       databaseURL: 'eraconverter-62594.firebaseapp.com'
     });*/
     firebase.initializeApp({
